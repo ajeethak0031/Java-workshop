f7a156c71ca714ce5cfa165fb934ea8b11e37305
Abarnahashini <abarnas24cse@vetias.ac.in>
2025-07-03 15:41:56 +0530

    new

--- src/com/java/workshop/temperaturedata/beans/Zone.java
+++ src/com/java/workshop/temperaturedata/beans/Zone.java
@@ -1,5 +1,25 @@
 package src.com.java.workshop.temperaturedata.beans;
 
-public class zone {
-
+public class Zone {
+    private String name;
+    private int zoneId;
+    private double area;
+    public void setName(String aName){
+        name=aName;
+    }
+    public String getaName(){
+        return name;
+    }
+    public void setZoneid(int aZoneid){
+        zoneId=aZoneid;
+    }
+    public int getaZoneid(){
+        return zoneId;
+    }
+    public void set(double  aArea){
+        area=aArea;
+    }    
+    public double getaArea(){
+        return area;
+    }
 }
