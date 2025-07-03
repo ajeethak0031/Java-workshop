fba35133505f1b5baffe71a2b3a3fbf5046efa0c
Abarnahashini <abarna24cse@vetias.ac.in>
2025-07-03 15:28:54 +0530

    temperaturedata

--- src/com/java/workshop/temperaturedata/beans/Floors.java
+++ src/com/java/workshop/temperaturedata/beans/Floors.java
@@ -1,20 +1,5 @@
 package src.com.java.workshop.temperaturedata.beans;
 
-public class Floors {
-    private String name;
-    private int number;
-    public void setName(String aName){
-        name=aName;
+public class Floor {
 
-    }
-    public String getName(){
-        return name;
-    }
-    public void setNumber(int  aNumber){
-        number=aNumber;
-
-    }
-    public int getNumber(){
-        return number;
-    }
 }
--- src/com/java/workshop/temperaturedata/beans/Zone.java
+++ src/com/java/workshop/temperaturedata/beans/Zone.java
@@ -1,25 +1,5 @@
 package src.com.java.workshop.temperaturedata.beans;
 
-public class Zone {
-    private String name;
-    private int zoneId;
-    private double area;
-    public void setName(String aName){
-        name=aName;
-    }
-    public String getaName(){
-        return name;
-    }
-    public void setZoneid(int aZoneid){
-        zoneId=aZoneid;
-    }
-    public int getaZoneid(){
-        return zoneId;
-    }
-    public void set(double  aArea){
-        area=aArea;
-    }    
-    public double getaArea(){
-        return area;
-    }
+public class zone {
+
 }
