3cb3878603ea2ca773c90dded02e6c9a5b4bd6f7
Abarnahashini <abarna24cse@vetias.ac.in>
2025-07-03 15:15:20 +0530

    sensor file

---
+++ src/com/java/workshop/temperaturedata/beans/sensor.java
@@ -0,0 +1,27 @@
+package src.com.java.workshop.temperaturedata.beans;
+
+public class sensor {
+    private int  sensorid;
+    private String sensorname;
+    private double temperature;
+    public void setSensorId(int aSensorId){
+        sensorid=aSensorId;
+
+    }
+    public int getSensorId(){
+        return sensorid;
+    }
+    public void setSensorName(String aSensorName){
+       sensorname=aSensorName;
+    }
+     public String getSensorName(String aSensorName){
+        return sensorname;
+
+    }
+    public void setTemperature(double aTemperature){
+        temperature=aTemperature;
+    }
+    public double getTemperature(){
+        return temperature;
+    }
+}
