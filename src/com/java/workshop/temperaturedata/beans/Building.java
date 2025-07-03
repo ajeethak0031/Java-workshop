581dc07507b676b3a1244722dddd1250888a8c91
Abarnahashini <abarna24cse@vetias.ac.in>
2025-07-03 15:21:14 +0530

    Temperaturedata

---
+++ src/com/java/workshop/temperaturedata/beans/Building.java
@@ -0,0 +1,52 @@
+package src.com.java.workshop.temperaturedata.beans;
+
+import java.time.LocalDateTime;
+
+public class Building {
+    private String name;
+    private double area;
+    private  int floors;
+    private LocalDateTime opendatetime;
+    private LocalDateTime closedatetime;
+    public void setName(String aName){
+        name=aName;
+
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public void setArea(double aArea){
+        area=aArea;
+
+    }
+    public double getArea()
+    {
+        return area;
+    }
+    public void setFloors(int aFloors){
+        floors=aFloors;
+
+    }
+    public int getFloors(){
+        return floors;
+    }
+    public void setOpenDateTime(LocalDateTime aOpDateTime){
+        opendatetime=aOpDateTime;
+
+    }
+    public LocalDateTime getaOpDateTime(){
+        return opendatetime;
+    }
+    public void setCloseDateTime(LocalDateTime aCloseDateTime){
+        closedatetime=aCloseDateTime;
+
+    }
+    public LocalDateTime getaCloseDateTime(){
+        return closedatetime;
+    }
+
+
+    
+    
+}
---
+++ src/com/java/workshop/temperaturedata/beans/acamedic.java
@@ -0,0 +1,7 @@
+package src.com.java.workshop.temperaturedata.beans;
+
+public class acamedic extends Building {
+    private boolean islibrary;
+    private boolean isauditoriumavailable;
+    
+}
