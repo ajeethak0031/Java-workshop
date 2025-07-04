af70f9414fd0332adfe8cae747fcc43886e497c4
ajeethak0031 <kathirvelajeetha@gmail.com>
2025-07-04 12:03:48 +0530

    temperaturesensor

---
+++ src/com/java/workshop/temperaturesensor.java
@@ -0,0 +1,14 @@
+public class Temperaturesensor implements Sensor {
+
+    private float temperature;
+    private Temperaturesensor(float aTemperature)
+    {
+        temperature=aTemperature;
+    }
+    public float getReading()
+    {
+        return temperature;
+    }
+
+
+}
