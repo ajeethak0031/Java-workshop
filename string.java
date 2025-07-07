9baea8117cf4d55529ef5ed548e674162f97a8dd
ajeethak0031 <kathirvelajeetha@gmail.com>
2025-07-07 10:53:12 +0530

    example

--- string.java
+++ string.java
@@ -1,1 +1,12 @@
+public class string{
+    public static void main (String[]args){
+        String firstname="ajee";
+        String lastname="kopss";
+        String Name=firstname.concat(lastname);
+        System.out.println(Name);
+        String finalname=Name.toUpperCase();
 
+    }
+    
+
+}
