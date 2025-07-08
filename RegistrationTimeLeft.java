fe0ecc23286ac74ecfbfae91f95ba63dd2513e7e
ajeethak0031 <kathirvelajeetha@gmail.com>
2025-07-08 10:49:23 +0530

    registrationtimeleft

---
+++ RegistrationTimeLeft.java
@@ -0,0 +1,14 @@
+
+import java.time.LocalTime;
+
+public class RegistrationTimeLeft {
+    public static void main(String[] args) {
+        LocalTime CurrentTime = LocalTime.now();
+        LocalTime registrationEndTime = LocalTime.of(18, 59, 59);
+        int hoursleft = registrationEndTime.getHour()-CurrentTime.getHour();
+        int minutesleft = registrationEndTime.getMinute()-CurrentTime.getMinute();
+        int secondsleft = registrationEndTime.getSecond()-CurrentTime.getSecond();
+        System.out.println("you have"+hoursleft + " Hour, " + minutesleft + "Minute," + secondsleft +" Second ");
+    }
+
+}
